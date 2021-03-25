6a3f1e23829a39d60d7b1c08947e5953c21a2308
Thumper <shaunwhl1991@gmail.com>
2021-03-25 11:37:27 +1100

    Update index.js
    - remove auth route as it has been merged into users

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,10 @@ const router = express.Router();
 
 /* Modular Routes */
 const frontend = require('./modules/frontend');
-// const auth = require('./modules/auth');  // For none-local passport strategy
 // const users = require('./modules/users');
 // const admin = require('./modules/admin');
 
 /* Routes Lists */
-// router.use('/auth', auth);
 // router.use('/users', users);
 // router.use('/admin', authenticatedAdmin, admin);
 router.use('/', /* authenticated, */ frontend);
